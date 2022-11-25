e357e75d146e295b9ebf4f26324a4a822b247d82
kielkow <matheuskiel@fiorifer.com.br>
2022-11-25 10:42:26 -0300

    feat: implement jogador dto and interface

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDTO } from './dtos/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
   @Post()
-  async criarAtualizarJogador() {
+  async criarAtualizarJogador(@Body() criarJogadorDTO: CriarJogadorDTO) {
+    const { nome, email, telefoneCelular } = criarJogadorDTO;
+
     return JSON.stringify({
-      message: 'post with success',
+      nome,
+      email,
+      telefoneCelular,
     });
   }
 }
