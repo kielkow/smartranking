5855a99a012f15a76fcd91acbd7130766c032fb3
kielkow <matheuskiel@fiorifer.com.br>
2022-12-15 14:43:57 -0300

    feat: config categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/smartranking'),
     JogadoresModule,
+    CategoriasModule,
   ],
   controllers: [],
   providers: [],
