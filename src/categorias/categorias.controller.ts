b64ce0ffb17afae74a0ea7e05f2e787a895e499a
kielkow <matheuskiel@fiorifer.com.br>
2022-12-16 12:08:52 -0300

    feat: criar categoria

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CategoriasService } from './categorias.service';
+import { CategoriaDTO } from './dtos/categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+  constructor(private readonly categoriasService: CategoriasService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() categoriaDTO: CategoriaDTO): Promise<Categoria> {
+    return await this.categoriasService.criarCategoria(categoriaDTO);
+  }
+}
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CategoriaDTO } from './dtos/categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
 
 @Injectable()
-export class CategoriasService {}
+export class CategoriasService {
+  constructor(
+    @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>,
+  ) {}
+
+  private readonly logger = new Logger(CategoriasService.name);
+
+  async criarCategoria(categoriaDTO: CategoriaDTO): Promise<Categoria> {
+    this.logger.log(`criarCategoria: ${JSON.stringify(categoriaDTO)}`);
+
+    const categoria = new this.categoriaModel(categoriaDTO);
+
+    const categoriaExiste = await this.categoriaModel
+      .findOne({ categoria: categoriaDTO.categoria })
+      .exec();
+
+    if (categoriaExiste) {
+      throw new BadRequestException('Categoria já existe');
+    }
+
+    return await categoria.save();
+  }
+}
