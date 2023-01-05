68f747d7e8a6af728285a27b804f44d17ed98d42
kielkow <matheuskiel@fiorifer.com.br>
2023-01-05 11:08:30 -0300

    feat: desafio interfaces

--- src/desafios/desafios.module.ts
+++ src/desafios/desafios.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { DesafiosController } from './desafios.controller';
 import { DesafiosService } from './desafios.service';
+import { DesafioSchema } from './interfaces/desafio.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Desafio',
+        schema: DesafioSchema,
+      },
+    ]),
+  ],
   controllers: [DesafiosController],
   providers: [DesafiosService],
 })
