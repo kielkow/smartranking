2ba03912e911d70c6fe53a747e337d36a0add894
kielkow <matheuskiel@fiorifer.com.br>
2023-01-05 10:47:32 -0300

    feat: config desafios module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/smartranking'),
     JogadoresModule,
     CategoriasModule,
+    DesafiosModule,
   ],
   controllers: [],
   providers: [],
