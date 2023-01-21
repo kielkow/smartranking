068222e119a98ef0465f77167b7a1d67c9f0f913
kielkow <matheuskiel@fiorifer.com.br>
2023-01-21 20:52:02 -0300

    feat: config rankings

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
 import { DesafiosModule } from './desafios/desafios.module';
 import { PartidasModule } from './partidas/partidas.module';
+import { RankingsModule } from './rankings/rankings.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PartidasModule } from './partidas/partidas.module';
     CategoriasModule,
     DesafiosModule,
     PartidasModule,
+    RankingsModule,
   ],
   controllers: [],
   providers: [],
