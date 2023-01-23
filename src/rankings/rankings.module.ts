d2175513d2a692c9385dc3bf6fdb57f91373152f
kielkow <matheuskiel@fiorifer.com.br>
2023-01-23 09:12:53 -0300

    feat: config ranking module

--- src/rankings/rankings.module.ts
+++ src/rankings/rankings.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { CategoriasModule } from 'src/categorias/categorias.module';
+import { JogadoresModule } from 'src/jogadores/jogadores.module';
+import { PartidasModule } from 'src/partidas/partidas.module';
+
+import { RankingSchema } from './interfaces/ranking.schema';
 import { RankingsController } from './rankings.controller';
 import { RankingsService } from './rankings.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Ranking',
+        schema: RankingSchema,
+      },
+    ]),
+    PartidasModule,
+    JogadoresModule,
+    CategoriasModule,
+  ],
   controllers: [RankingsController],
   providers: [RankingsService],
 })
