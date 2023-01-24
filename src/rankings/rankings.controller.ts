f176d98a3254c292f07c6bf3a16dd6e8d5d3ad50
kielkow <matheuskiel@fiorifer.com.br>
2023-01-24 11:19:52 -0300

    feat: consultar rankings

--- src/rankings/rankings.controller.ts
+++ src/rankings/rankings.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('rankings')
-export class RankingsController {}
+import { Ranking } from './interfaces/ranking.interface';
+
+import { RankingsService } from './rankings.service';
+
+@Controller('api/v1/rankings')
+export class RankingsController {
+  constructor(private readonly rankingsService: RankingsService) {}
+
+  @Get()
+  async consultarRankings(): Promise<Ranking[]> {
+    return await this.rankingsService.consultarRankings();
+  }
+}
--- src/rankings/rankings.service.ts
+++ src/rankings/rankings.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Ranking } from './interfaces/ranking.interface';
+
+import { DesafiosService } from 'src/desafios/desafios.service';
+import { PartidasService } from 'src/partidas/partidas.service';
+import { JogadoresService } from 'src/jogadores/jogadores.service';
+import { CategoriasService } from 'src/categorias/categorias.service';
 
 @Injectable()
-export class RankingsService {}
+export class RankingsService {
+  constructor(
+    @InjectModel('Ranking') private readonly rankingModel: Model<Ranking>,
+    private readonly desafiosService: DesafiosService,
+    private readonly partidasService: PartidasService,
+    private readonly jogadoresService: JogadoresService,
+    private readonly categoriasService: CategoriasService,
+  ) {}
+
+  private readonly logger = new Logger(RankingsService.name);
+
+  async consultarRankings(): Promise<Ranking[]> {
+    return await this.rankingModel
+      .find()
+      .populate('desafio')
+      .populate('partida')
+      .populate('categoria')
+      .populate('jogador')
+      .exec();
+  }
+}
