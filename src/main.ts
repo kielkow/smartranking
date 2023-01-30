7806b2a155dd52c130e5352e6486c84dd0f582a7
kielkow <matheuskiel@fiorifer.com.br>
2023-01-30 09:36:34 -0300

    feat: add moment-timezone to dates

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import momentTimezone from 'moment-timezone';
+
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
@@ -6,6 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
 
   await app.listen(3000);
 }
