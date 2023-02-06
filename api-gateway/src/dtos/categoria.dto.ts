3e23272aa6cb4f16c6c8b77aca18ffa813731d20
kielkow <matheuskiel@fiorifer.com.br>
2023-02-06 09:36:45 -0300

    feat: import categoria DTO to api-gateway

---
+++ api-gateway/src/dtos/categoria.dto.ts
@@ -0,0 +1,21 @@
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from 'class-validator';
+
+export class CategoriaDTO {
+  @IsString()
+  @IsNotEmpty()
+  readonly categoria: string;
+
+  @IsString()
+  @IsNotEmpty()
+  descricao: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  eventos: Array<Evento>;
+}
+
+interface Evento {
+  nome: string;
+  operacao: string;
+  valor: number;
+}
