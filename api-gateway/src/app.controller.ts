a2f70faedb7cfd1ee26538fb7c6ddd67529c31c3
kielkow <matheuskiel@fiorifer.com.br>
2023-02-06 10:00:14 -0300

    feat: config rabbitmq

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
 
-@Controller()
-export class AppController {}
+import { CategoriaDTO } from './dtos/categoria.dto';
+
+@Controller('api/v1')
+export class AppController {
+  private logger = new Logger(AppController.name);
+
+  private clientAdminBackend: ClientProxy;
+
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['http://guest:guest@localhost:15672/smartranking'],
+        queue: 'admin-backend',
+      },
+    });
+  }
+
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() categoriaDTO: CategoriaDTO): Promise<any> {
+    this.logger.log(`criar-categoria: ${JSON.stringify(categoriaDTO)}`);
+
+    return this.clientAdminBackend.emit('criar-categoria', categoriaDTO);
+  }
+}
