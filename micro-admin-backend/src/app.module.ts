f7d3e39dfcd9d289dd313ffe4edb62cd73ac683b
kielkow <matheuskiel@fiorifer.com.br>
2023-02-07 11:13:50 -0300

    feat: consume message criar-categoria

--- micro-admin-backend/src/app.module.ts
+++ micro-admin-backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { CategoriaSchema } from './interfaces/categorias/categoria.schema';
+import { JogadorSchema } from './interfaces/jogadores/jogador.schema';
+
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/smartranking'),
+    MongooseModule.forFeature([
+      { name: 'Categoria', schema: CategoriaSchema },
+      { name: 'Jogador', schema: JogadorSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
