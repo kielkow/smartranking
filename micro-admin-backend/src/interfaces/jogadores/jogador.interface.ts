b4712388d3c147b30c40eb41f8016f1a5ae7cf3b
kielkow <matheuskiel@fiorifer.com.br>
2023-02-07 10:02:10 -0300

    feat: categoria/jogador interface e schema

---
+++ micro-admin-backend/src/interfaces/jogadores/jogador.interface.ts
@@ -0,0 +1,15 @@
+import { Document } from 'mongoose';
+
+export interface Jogador extends Document {
+  readonly telefoneCelular: string;
+
+  readonly email: string;
+
+  nome: string;
+
+  ranking: string;
+
+  posicaoRanking: string;
+
+  urlFotoJogador: string;
+}
