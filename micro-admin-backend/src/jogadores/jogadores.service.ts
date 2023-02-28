d6126b335cebd6657987a1cd84e444f6a585a99b
kielkow <matheuskiel@fiorifer.com.br>
2023-02-28 10:12:44 -0300

    feat: micro-admin-backend jogadores service

---
+++ micro-admin-backend/src/jogadores/jogadores.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Jogador } from './interfaces/jogador.interface';
+
+@Injectable()
+export class JogadoresService {
+  constructor(
+    @InjectModel('Jogador') private readonly jogadorModel: Model<Jogador>,
+  ) {}
+
+  private readonly logger = new Logger(JogadoresService.name);
+
+  async criarJogador(jogador: Jogador): Promise<Jogador> {
+    try {
+      this.logger.log(JSON.stringify(jogador));
+
+      const jogadorCriada = new this.jogadorModel(jogador);
+
+      return await jogadorCriada.save();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      throw new RpcException(error.message);
+    }
+  }
+
+  async consultarJogadores(): Promise<Jogador[]> {
+    try {
+      return await this.jogadorModel.find().exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      throw new RpcException(error.message);
+    }
+  }
+
+  async consultarJogadorPorID(id: string): Promise<Jogador> {
+    try {
+      return await this.jogadorModel.findById(id).exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      throw new RpcException(error.message);
+    }
+  }
+
+  async atualizarJogador(id: string, jogador: any): Promise<void> {
+    try {
+      this.logger.log(JSON.stringify({ id, jogador }));
+
+      await this.jogadorModel
+        .findOneAndUpdate({ _id: id }, { $set: jogador })
+        .exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      throw new RpcException(error.message);
+    }
+  }
+
+  async deletarJogador(id: string): Promise<void> {
+    try {
+      this.logger.log(id);
+
+      await this.jogadorModel.findByIdAndDelete({ _id: id }).exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      throw new RpcException(error.message);
+    }
+  }
+}
