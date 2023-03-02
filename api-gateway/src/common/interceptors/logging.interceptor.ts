6301d7236b4d012f80553f647fcce51acb5fd866
kielkow <matheuskiel@fiorifer.com.br>
2023-03-02 13:09:12 -0300

    feat: interceptor com mais informações

--- api-gateway/src/common/interceptors/logging.interceptor.ts
+++ api-gateway/src/common/interceptors/logging.interceptor.ts
@@ -12,12 +12,33 @@ export class LoggingInterceptor implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    console.log('before...');
+    console.log({
+      context: {
+        type: context.getType(),
+        class: context.getClass(),
+        handler: context.getHandler(),
+        requestData: {
+          date: new Date().toISOString(),
+          method: context.getArgByIndex(0)['method'],
+          originalUrl: context.getArgByIndex(0)['originalUrl'],
+          url: context.getArgByIndex(0)['url'],
+          body: JSON.stringify(context.getArgByIndex(0)['body']),
+          params: JSON.stringify(context.getArgByIndex(0)['params']),
+          query: JSON.stringify(context.getArgByIndex(0)['query']),
+        },
+      },
+    });
 
-    const now = Date.now();
-
-    return next
-      .handle()
-      .pipe(tap(() => console.log(`after... ${Date.now() - now}ms`)));
+    return next.handle().pipe(
+      tap((value) =>
+        console.log({
+          responseData: {
+            date: new Date().toISOString(),
+            success: true,
+            body: JSON.stringify(value),
+          },
+        }),
+      ),
+    );
   }
 }
