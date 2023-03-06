af2eab8aec64c406a3a086d0542b9f312f711cbe
kielkow <matheuskiel@fiorifer.com.br>
2023-03-06 10:12:03 -0300

    feat: api-gateway - aws-service uploadArquivo

--- api-gateway/src/aws/aws.service.ts
+++ api-gateway/src/aws/aws.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
-export class AwsService {}
+export class AwsService {
+  private logger = new Logger(AwsService.name);
+
+  public async uploadArquivo(file: any, id: string) {
+    const s3 = new AWS.S3({
+      region: 'sa-east-1',
+      accessKeyId: '',
+      secretAccessKey: '',
+    });
+
+    const fileExtension = file.originalname.split('.')[1];
+
+    const urlKey = `${id}.${fileExtension}`;
+
+    this.logger.log(urlKey);
+
+    const data = s3
+      .putObject({
+        Body: file.buffer,
+        Bucket: 'smartranking',
+        Key: urlKey,
+      })
+      .promise()
+      .then(
+        (data) => {
+          return data;
+        },
+        (error) => {
+          this.logger.error(error);
+          return error;
+        },
+      );
+
+    return data;
+  }
+}
