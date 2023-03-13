4a5d246b0230be21667b5f447c6db05be9be28b4
kielkow <matheuskiel@fiorifer.com.br>
2023-03-13 11:27:58 -0300

    feat: api-gateway - criarDesafio

--- api-gateway/src/desafios/desafios.controller.ts
+++ api-gateway/src/desafios/desafios.controller.ts
@@ -1,6 +1,18 @@
-import { Controller, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 
 import { ClientProxyFactoryProvider } from 'src/common/providers/client-proxy/client-proxy-provider-factory';
+import { Jogador } from 'src/jogadores/interfaces/jogador.interface';
+import { Categoria } from 'src/categorias/interfaces/categoria.interface';
+import { DesafioDTO } from './dtos/desafio.dto';
 
 @Controller('api/v1/desafios')
 export class DesafiosController {
@@ -13,4 +25,60 @@ export class DesafiosController {
 
   private clientAdminBackendDesafios =
     this.clientProxyFactoryProvider.getClientProxyInstanceDesafios();
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async criarDesafio(@Body() desafioDTO: DesafioDTO) {
+    this.logger.log(`criar-desafio: ${JSON.stringify(desafioDTO)}`);
+
+    // VERIFICAR SE A CATEGORIA EXISTE
+    const categoria: Categoria = await lastValueFrom(
+      this.clientAdminBackend.send(
+        'consultar-categorias',
+        desafioDTO.categoria,
+      ),
+    );
+    if (!categoria) throw new BadRequestException(`Categoria não encontrada`);
+
+    // VERIFICAR SE OS JOGADORES EXISTEM
+    const jogadorID1 = desafioDTO.jogadores[0];
+    const jogadorID2 = desafioDTO.jogadores[1];
+
+    const jogador1: Jogador = await lastValueFrom(
+      this.clientAdminBackend.send('consultar-jogadores', jogadorID1),
+    );
+    if (!jogador1) {
+      throw new BadRequestException(`Jogador ${jogadorID1} não encontrado`);
+    }
+
+    const jogador2: Jogador = await lastValueFrom(
+      this.clientAdminBackend.send('consultar-jogadores', jogadorID2),
+    );
+    if (!jogador2) {
+      throw new BadRequestException(`Jogador ${jogadorID2} não encontrado`);
+    }
+
+    // VERIFICAR SE A CATEGORIA É A MESMA DOS JOGADORES
+    if (
+      categoria._id !== jogador1.categoria ||
+      categoria._id !== jogador2.categoria
+    ) {
+      throw new BadRequestException(
+        'A categoria informada não é a mesma dos jogadores',
+      );
+    }
+
+    // VERIFICAR SE O SOLICITANTE É UM DOS JOGADORES INFORMADOS
+    if (
+      desafioDTO.solicitante !== jogadorID1 &&
+      desafioDTO.solicitante !== jogadorID2
+    ) {
+      throw new BadRequestException(
+        'O jogador solicitante não é um dos jogadores informados no desafio',
+      );
+    }
+
+    // CRIAR DESAFIO
+    this.clientAdminBackendDesafios.emit('criar-desafio', desafioDTO);
+  }
 }
