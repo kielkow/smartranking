c7255117cb13bb49a1cc26031979b03bfced723d
kielkow <matheuskiel@fiorifer.com.br>
2023-03-17 11:20:16 -0300

    feat: micro-desafios - config main modules

--- micro-desafios/src/app.module.ts
+++ micro-desafios/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { DesafiosModule } from './desafios/desafios.module';
+import { PartidasModule } from './partidas/partidas.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/smartranking'),
+    DesafiosModule,
+    PartidasModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- micro-desafios/src/main.ts
+++ micro-desafios/src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
 
+const logger = new Logger('Main');
+
+const configService = new ConfigService();
+const RABBITMQ_URL = configService.get<string>('RABBITMQ_URL');
+const RABBITMQ_USER = configService.get<string>('RABBITMQ_USER');
+const RABBITMQ_PASSWORD = configService.get<string>('RABBITMQ_PASSWORD');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@${RABBITMQ_URL}`],
+      noAck: false,
+      queue: 'admin-backend-desafios',
+    },
+  });
+
+  await app.listen().then(() => logger.log('microservice is listening'));
 }
 bootstrap();
