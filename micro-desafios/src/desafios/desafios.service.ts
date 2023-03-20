6cfa74c8685654ad0aa93e3ef3f7c937839d0eeb
kielkow <matheuskiel@fiorifer.com.br>
2023-03-20 11:46:54 -0300

    feat: micro-desafio - criarDesafio service

--- micro-desafios/src/desafios/desafios.service.ts
+++ micro-desafios/src/desafios/desafios.service.ts
@@ -1,6 +1,30 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+
+import { Desafio } from './interfaces/desafio.interface';
 
 @Injectable()
 export class DesafiosService {
+  constructor(
+    @InjectModel('Desafio') private readonly desafioModel: Model<Desafio>,
+  ) {}
+
   private readonly logger = new Logger(DesafiosService.name);
+
+  async criarDesafio(desafio: Desafio): Promise<Desafio> {
+    try {
+      this.logger.log(JSON.stringify(desafio));
+
+      const desafioCriado = new this.desafioModel(desafio);
+
+      return await desafioCriado.save();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      throw new RpcException(error.message);
+    }
+  }
 }
