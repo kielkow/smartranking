4082c4d21f3bf496c6ecf249dc2c40590888b71f
kielkow <matheuskiel@fiorifer.com.br>
2023-03-20 11:46:46 -0300

    feat: micro-desafio - criarDesafio controller

--- micro-desafios/src/desafios/desafios.controller.ts
+++ micro-desafios/src/desafios/desafios.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Logger } from '@nestjs/common';
+import { EventPattern, Payload, Ctx, RmqContext } from '@nestjs/microservices';
 
 import { DesafiosService } from './desafios.service';
+import { Desafio } from './interfaces/desafio.interface';
 
 const ackErrors: string[] = ['E11000'];
 
@@ -9,4 +11,26 @@ export class DesafiosController {
   constructor(private readonly desafiosService: DesafiosService) {}
 
   logger = new Logger(DesafiosController.name);
+
+  @EventPattern('criar-desafio')
+  async criarDesafio(@Payload() desafio: Desafio, @Ctx() context: RmqContext) {
+    this.logger.log(`desafio: ${JSON.stringify(desafio)}`);
+
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      await this.desafiosService.criarDesafio(desafio);
+
+      await channel.ack(originalMessage);
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      const filterAckError = ackErrors.filter((ackError) =>
+        error.message.includes(ackError),
+      );
+
+      if (filterAckError) await channel.ack(originalMessage);
+    }
+  }
 }
