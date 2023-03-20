ab7c35611c1854a8aa16d9d826166fce16d1ccd8
kielkow <matheuskiel@fiorifer.com.br>
2023-03-20 11:50:01 -0300

    feat: micro-desafio - import desafio schema

--- micro-desafios/src/desafios/desafios.module.ts
+++ micro-desafios/src/desafios/desafios.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { DesafiosController } from './desafios.controller';
 import { DesafiosService } from './desafios.service';
+import { DesafioSchema } from './interfaces/desafio.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Desafio',
+        schema: DesafioSchema,
+      },
+    ]),
+  ],
   controllers: [DesafiosController],
   providers: [DesafiosService],
   exports: [DesafiosService],
