1a9cd4520e18ec1fe81f3fbe04e44ad50107a180
kielkow <matheuskiel@fiorifer.com.br>
2023-03-24 10:51:50 -0300

    feat: micro-desafio - criar partida controller

--- micro-desafios/src/partidas/partidas.controller.ts
+++ micro-desafios/src/partidas/partidas.controller.ts
@@ -1,4 +1,7 @@
 import { Controller, Logger } from '@nestjs/common';
+import { EventPattern, Payload, Ctx, RmqContext } from '@nestjs/microservices';
+
+import { Partida } from './interfaces/partida.interface';
 
 import { PartidasService } from './partidas.service';
 
@@ -9,4 +12,26 @@ export class PartidasController {
   constructor(private readonly partidasService: PartidasService) {}
 
   logger = new Logger(PartidasController.name);
+
+  @EventPattern('criar-partida')
+  async criarPartida(@Payload() partida: Partida, @Ctx() context: RmqContext) {
+    this.logger.log(`partida: ${JSON.stringify(partida)}`);
+
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      await this.partidasService.criarPartida(partida);
+
+      await channel.ack(originalMessage);
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      const filterAckError = ackErrors.filter((ackError) =>
+        error.message.includes(ackError),
+      );
+
+      if (filterAckError) await channel.ack(originalMessage);
+    }
+  }
 }
