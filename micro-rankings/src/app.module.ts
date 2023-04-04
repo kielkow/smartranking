0d36176499332e51aad38edb95455e41da981ec3
kielkow <matheuskiel@fiorifer.com.br>
2023-04-04 12:13:29 -0300

    chore: micro-rankings - config micro-rankings

--- micro-rankings/src/app.module.ts
+++ micro-rankings/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { RankingsModule } from './rankings/rankings.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/srrankings'),
+    RankingsModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })
