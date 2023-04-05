58ef307e37a5700c66807f80750a2a0a95a036e6
kielkow <matheuskiel@fiorifer.com.br>
2023-04-05 11:10:35 -0300

    feat: micro-rankings - rankings schema

--- micro-rankings/src/rankings/rankings.module.ts
+++ micro-rankings/src/rankings/rankings.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { RankingSchema } from './interfaces/ranking.schema';
 import { RankingsController } from './rankings.controller';
 import { RankingsService } from './rankings.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: 'Ranking', schema: RankingSchema }]),
+  ],
   controllers: [RankingsController],
   providers: [RankingsService],
   exports: [RankingsService],
