dad069bf33c084a126149520534c6400bd1953f1
kielkow <matheuskiel@fiorifer.com.br>
2023-04-06 12:13:26 -0300

    wip: micro-rankings - processar partida

--- micro-rankings/src/rankings/rankings.controller.ts
+++ micro-rankings/src/rankings/rankings.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 
-@Controller('rankings')
-export class RankingsController {}
+import { RankingsService } from './rankings.service';
+
+const ackErrors: string[] = ['E11000'];
+
+@Controller()
+export class RankingsController {
+  constructor(private readonly rankingsService: RankingsService) {}
+
+  private readonly logger = new Logger(RankingsController.name);
+
+  @EventPattern('processar-partida')
+  async processarPartida(
+    @Payload() partidaId: string,
+    @Ctx() context: RmqContext,
+  ) {
+    this.logger.log(`processar-partida: ${JSON.stringify(partidaId)}`);
+
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      await this.rankingsService.processarPartida(partidaId);
+
+      await channel.ack(originalMessage);
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      const filterAckError = ackErrors.filter((ackError) =>
+        error.message.includes(ackError),
+      );
+
+      if (filterAckError) await channel.ack(originalMessage);
+    }
+  }
+}
--- micro-rankings/src/rankings/rankings.service.ts
+++ micro-rankings/src/rankings/rankings.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
+import { lastValueFrom } from 'rxjs';
+
+import { ClientProxyFactoryProvider } from 'src/proxyrmq/client-proxy';
+import { Ranking } from './interfaces/ranking.schema';
 
 @Injectable()
-export class RankingsService {}
+export class RankingsService {
+  constructor(
+    @InjectModel('Ranking') private readonly rankingModel: Model<Ranking>,
+    private clientProxyFactoryProvider: ClientProxyFactoryProvider,
+  ) {}
+
+  private readonly logger = new Logger(RankingsService.name);
+
+  private clientProxyDesafios =
+    this.clientProxyFactoryProvider.getClientProxyInstanceDesafios();
+
+  async processarPartida(partidaId: string): Promise<void> {
+    try {
+      this.logger.log(`partidaID: ${partidaId}`);
+
+      // CONSULTA A PARTIDA
+      const partida = await lastValueFrom(
+        this.clientProxyDesafios.send('consultar-partidas', partidaId),
+      );
+
+      this.logger.log(`partida: ${JSON.stringify(partida)}`);
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      throw new RpcException(error.message);
+    }
+  }
+}
