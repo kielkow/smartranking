8eb600acb75bb87a794ba7bf3c5b9c14934e61e5
kielkow <matheuskiel@fiorifer.com.br>
2023-04-11 10:50:24 -0300

    feat: micro-rankings - config client proxy

--- micro-rankings/src/app.module.ts
+++ micro-rankings/src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
 import { RankingsModule } from './rankings/rankings.module';
+import { ClientProxyModule } from './proxyrmq/proxyrmq.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/srrankings'),
     RankingsModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ClientProxyModule,
   ],
   controllers: [],
   providers: [],
--- micro-rankings/src/rankings/rankings.module.ts
+++ micro-rankings/src/rankings/rankings.module.ts
@@ -4,10 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RankingSchema } from './interfaces/ranking.schema';
 import { RankingsController } from './rankings.controller';
 import { RankingsService } from './rankings.service';
+import { ClientProxyModule } from 'src/proxyrmq/proxyrmq.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Ranking', schema: RankingSchema }]),
+    ClientProxyModule,
   ],
   controllers: [RankingsController],
   providers: [RankingsService],
