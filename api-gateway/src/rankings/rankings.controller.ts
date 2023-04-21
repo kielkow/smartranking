4dec3274dc9740b8b671c46a5ecc26081ddf00b9
kielkow <matheuskiel@fiorifer.com.br>
2023-04-21 16:42:04 -0300

    refact: api-gateway - rankings

--- api-gateway/src/rankings/rankings.controller.ts
+++ api-gateway/src/rankings/rankings.controller.ts
@@ -1,48 +1,16 @@
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Logger,
-  Query,
-} from '@nestjs/common';
-
-import { Observable } from 'rxjs';
-
-import { ClientProxyFactoryProvider } from 'src/common/providers/client-proxy/client-proxy-provider-factory';
+import { Controller, Get, Query } from '@nestjs/common';
 
 import { RankingsService } from './rankings.service';
 
 @Controller('api/v1/rankings')
 export class RankingsController {
-  private logger = new Logger(RankingsController.name);
-
-  constructor(
-    private clientProxyFactoryProvider: ClientProxyFactoryProvider,
-    private rankingsService: RankingsService,
-  ) {}
-
-  private clientProxyRankings =
-    this.clientProxyFactoryProvider.getClientProxyInstanceRankings();
+  constructor(private rankingsService: RankingsService) {}
 
   @Get()
-  consultarCategorias(
+  consultarRankings(
     @Query('categoriaId') categoriaId: string,
     @Query('dataRef') dataRef: string,
-  ): Observable<any> {
-    this.logger.log(
-      `consultar-rankings: ${JSON.stringify({
-        categoriaId,
-        dataRef,
-      })}`,
-    );
-
-    if (!categoriaId) {
-      throw new BadRequestException('O ID da categoria deve ser informado');
-    }
-
-    return this.clientProxyRankings.send('consultar-rankings', {
-      categoriaId,
-      dataRef,
-    });
+  ) {
+    return this.rankingsService.consultarRankings(categoriaId, dataRef);
   }
 }
--- api-gateway/src/rankings/rankings.service.ts
+++ api-gateway/src/rankings/rankings.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+import { ClientProxyFactoryProvider } from 'src/common/providers/client-proxy/client-proxy-provider-factory';
 
 @Injectable()
-export class RankingsService {}
+export class RankingsService {
+  private logger = new Logger(RankingsService.name);
+
+  constructor(private clientProxyFactoryProvider: ClientProxyFactoryProvider) {}
+
+  private clientProxyRankings =
+    this.clientProxyFactoryProvider.getClientProxyInstanceRankings();
+
+  consultarRankings(categoriaId: string, dataRef: string): Observable<any> {
+    this.logger.log(
+      `consultar-rankings: ${JSON.stringify({
+        categoriaId,
+        dataRef,
+      })}`,
+    );
+
+    if (!categoriaId) {
+      throw new BadRequestException('O ID da categoria deve ser informado');
+    }
+
+    return this.clientProxyRankings.send('consultar-rankings', {
+      categoriaId,
+      dataRef,
+    });
+  }
+}
