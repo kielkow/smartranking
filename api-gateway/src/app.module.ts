16d7fc0241221e0cefc52079121fd21102d8757a
kielkow <matheuskiel@fiorifer.com.br>
2023-04-25 11:04:18 -0300

    feat: api-gateway - setup auth module

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { CategoriasModule } from './categorias/categorias.module';
-import { JogadoresModule } from './jogadores/jogadores.module';
-import { DesafiosModule } from './desafios/desafios.module';
-
-import { ClientProxyFactoryProvider } from './common/providers/client-proxy/client-proxy-provider-factory';
-import { ClientProxyModule } from './common/providers/client-proxy/client-proxy.module';
 import { AwsModule } from './aws/aws.module';
+import { AuthModule } from './auth/auth.module';
 import { RankingsModule } from './rankings/rankings.module';
+import { DesafiosModule } from './desafios/desafios.module';
+import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
+
+import { ClientProxyModule } from './common/providers/client-proxy/client-proxy.module';
+import { ClientProxyFactoryProvider } from './common/providers/client-proxy/client-proxy-provider-factory';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { RankingsModule } from './rankings/rankings.module';
     RankingsModule,
     ClientProxyModule,
     AwsModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [],
