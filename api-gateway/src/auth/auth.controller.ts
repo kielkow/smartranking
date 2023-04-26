8e5d01a2e595c9df7d93854cf46c5c8ec8ed9544
kielkow <matheuskiel@fiorifer.com.br>
2023-04-26 11:36:48 -0300

    feat: api-gateway - auth controller - registrar usuario

--- api-gateway/src/auth/auth.controller.ts
+++ api-gateway/src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('auth')
-export class AuthController {}
+import { AuthRegistroUsuarioDTO } from './dtos/auth-registro-usuario.dto';
+import { AuthLoginUsuarioDTO } from './dtos/auth-login-usuario.dto';
+
+import { AwsCognitoService } from 'src/aws/aws-cognito.service';
+
+@Controller('api/v1/auth')
+export class AuthController {
+  constructor(private awsCognitoService: AwsCognitoService) {}
+
+  @Post('/registro')
+  async registro(@Body() registroDTO: AuthRegistroUsuarioDTO) {
+    await this.awsCognitoService.registrarUsuario(registroDTO);
+  }
+
+  @Post('/login')
+  async login(@Body() loginDTO: AuthLoginUsuarioDTO) {
+    console.log(loginDTO);
+    return;
+  }
+}
--- api-gateway/src/auth/auth.module.ts
+++ api-gateway/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AwsModule } from 'src/aws/aws.module';
 
 @Module({
+  imports: [AwsModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
