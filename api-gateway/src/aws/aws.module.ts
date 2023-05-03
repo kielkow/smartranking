a66a83a1e61a304f8495fa0e3d3d42a23c02412c
kielkow <matheuskiel@fiorifer.com.br>
2023-05-03 14:20:14 -0300

    fix: api-gateway - export aws providers

--- api-gateway/src/aws/aws.module.ts
+++ api-gateway/src/aws/aws.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { AwsS3Service } from './aws-s3.service';
 import { AwsCognitoService } from './aws-cognito.service';
 
+import { AwsS3Config } from './aws-s3.config';
+import { AwsCognitoConfig } from './aws-cognito.config';
+
 @Module({
   imports: [],
   controllers: [],
-  providers: [AwsS3Service, AwsCognitoService],
-  exports: [AwsS3Service, AwsCognitoService],
+  providers: [AwsS3Service, AwsS3Config, AwsCognitoService, AwsCognitoConfig],
+  exports: [AwsS3Service, AwsS3Config, AwsCognitoService, AwsCognitoConfig],
 })
 export class AwsModule {}
